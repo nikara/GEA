4e9de53f51580ab828cfb9dfb5f2bd755bb0924f
Nikara <nickolaslamin@gmail.com>
2026-02-25 18:45:52 -0300

    Salvar alterações locais antes do merge

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-import http from "http";
+//import http from "http";
+
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso De Node.js",
-    "/livros":"Entrei na rota livros",
-    "/autores":"Qualquer coisa"
-};
-
-const server = http.createServer((req,res) => {
-    res.writeHead(200,{"Content-Type": "text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT,() =>{
+app.listen(PORT,() =>{
     console.log("Servidor escutando !");
 });
 
